6ec582525e4ff72144f94eb65f3fe4bc479073f2
oceanstats <thistrove@yahoo.co.uk>
2017-08-02 15:28:14 +0100

    Added rainfall conversion module

---
+++ rainfall_conversion.py
@@ -0,0 +1,10 @@
+'''A library to perform rainfall analysis'''
+
+def inches_to_mm(inches)
+	'''convertinches to mm
+	
+	Arguments:
+	inches -- the rainfall in inches
+	'''
+	mm=inches*25.4
+	return mm
